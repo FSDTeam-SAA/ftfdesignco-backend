4d062be08f3903955244b949d8be126c9366c37b
Sajjad177 <sajjadhossainx06@gmail.com>
2025-06-17 15:29:32 +0600

    Create utility functions for handling image uploads and deletions.

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -17,8 +17,73 @@ const createNewAccount = async (req, res) => {
   }
 };
 
+const verifyEmail = async (req, res) => {
+  try {
+    const { email } = req.user;
+    const result = await userService.verifyUserEmail(req.body, email);
+    return res.status(200).json({
+      success: true,
+      message: "Email verified successfully",
+      data: result,
+    });
+  } catch (error) {
+    return res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await userService.getAllUsersFromDb();
+    return res.status(200).json({
+      success: true,
+      message: "Users retrieved successfully",
+      data: users,
+    });
+  } catch (error) {
+    return res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+const getMyProfile = async (req, res) => {
+  try {
+    const { email } = req.user;
+    const user = await userService.getMyProfileFromDb({ email });
+
+    return res.status(200).json({
+      success: true,
+      message: "User profile retrieved successfully",
+      data: user,
+    });
+  } catch (error) {
+    return res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+const updateUserProfile = async (req, res) => {
+  try {
+    const { email } = req.user;
+    const result = await userService.updateUserProfile(
+      req.body,
+      email,
+      req.file
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "User profile updated successfully",
+      data: result,
+    });
+  } catch (error) {
+    return res.status(400).json({ success: false, message: error.message });
+  }
+};
+
 const userController = {
   createNewAccount,
+  verifyEmail,
+  getAllUsers,
+  getMyProfile,
+  updateUserProfile,
 };
 
 module.exports = userController;
--- src/modules/user/user.service.js
+++ src/modules/user/user.service.js
@@ -1,5 +1,7 @@
 const config = require("../../config");
+const { sendImageToCloudinary } = require("../../utils/cloudnary");
 const sendEmail = require("../../utils/sendEmail");
+const { createToken } = require("../../utils/tokenGenerate");
 const verificationCodeTemplate = require("../../utils/verificationCodeTemplate");
 const User = require("./user.model");
 const bcrypt = require("bcrypt");
@@ -8,6 +10,10 @@ const createNewAccountInDB = async (payload) => {
   const existingUser = await User.findOne({ email: payload.email });
   if (existingUser) {
     throw new Error("User already exists");
+  }
+
+  if (payload.password.length < 6) {
+    throw new Error("Password must be at least 6 characters long");
   }
 
   const otp = Math.floor(100000 + Math.random() * 900000).toString();
@@ -49,16 +55,94 @@ const createNewAccountInDB = async (payload) => {
   );
 
   return {
-    _id: result._id,
-    name: result.name,
-    email: result.email,
-    role: result.role,
+    user: {
+      _id: result._id,
+      name: result.name,
+      email: result.email,
+      role: result.role,
+    },
     accessToken,
   };
 };
 
+const verifyUserEmail = async (payload, email) => {
+  const { otp } = payload;
+  if (!otp) throw new Error("OTP is required");
+
+  const existingUser = await User.findOne({ email });
+  if (!existingUser) throw new Error("User not found");
+
+  if (!existingUser.otp || !existingUser.otpExpires) {
+    throw new Error("OTP not requested or expired");
+  }
+
+  if (existingUser.otpExpires < new Date()) {
+    throw new Error("OTP has expired");
+  }
+
+  if (existingUser.isVerified === true) {
+    throw new Error("User already verified");
+  }
+
+  const isOtpMatched = await bcrypt.compare(otp.toString(), existingUser.otp);
+  if (!isOtpMatched) throw new Error("Invalid OTP");
+
+  const result = await User.findOneAndUpdate(
+    { email },
+    {
+      isVerified: true,
+      $unset: { otp: "", otpExpires: "" },
+    },
+    { new: true }
+  ).select("-password -otp -otpExpires");
+  return result;
+};
+
+
+const getAllUsersFromDb = async () => {
+  const users = await User.find({ isVerified: true }).select(
+    "-password -otp -otpExpires"
+  );
+  return users;
+};
+
+
+
+const getMyProfileFromDb = async (email) => {
+  const user = await User.findOne(email).select("-password -otp -otpExpires");
+  if (!user) throw new Error("User not found");
+  return user;
+};
+
+
+const updateUserProfile = async (payload, email, file) => {
+  const isExistingUser = await User.findOne({ email });
+  if (!isExistingUser) throw new Error("User not found");
+
+  if (file) {
+    const imageName = `${Date.now()}-${file.originalname}`;
+    const path = file?.path;
+    const { secure_url } = await sendImageToCloudinary(imageName, path);
+    payload.imageLink = secure_url;
+  }
+
+  const updatedUser = await User.findOneAndUpdate(
+    {
+      email,
+    },
+    payload,
+    { new: true }
+  ).select("-password -otp -otpExpires");
+  return updatedUser;
+};
+
+
 const userService = {
   createNewAccountInDB,
+  verifyUserEmail,
+  getAllUsersFromDb,
+  getMyProfileFromDb,
+  updateUserProfile,
 };
 
 module.exports = userService;
