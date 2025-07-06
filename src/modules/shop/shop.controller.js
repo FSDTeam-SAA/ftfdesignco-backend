b9d05605ae6bcc90f8d87aee71c78baf9dc40afe
sajjad <sajjadsajjad098765@gmail.com>
2025-07-06 19:55:10 +0600

    Remove commented-out addProductInShop functionality and update shop model to include status field

--- src/modules/shop/shop.controller.js
+++ src/modules/shop/shop.controller.js
@@ -95,27 +95,6 @@ const getAllShops = async (req, res) => {
   }
 };
 
-// const addProductInShop = async (req, res) => {
-//   try {
-//     const { productId } = req.body;
-//     const { email } = req.user;
-
-//     const result = await shopService.addProductToShop(productId, email);
-
-//     return res.status(200).json({
-//       success: true,
-//       code: 200,
-//       message: "Product added to shop successfully",
-//       data: result,
-//     });
-//   } catch (error) {
-//     return res.status(400).json({
-//       success: false,
-//       code: 400,
-//       message: error.message,
-//     });
-//   }
-// };
 
 const shopController = {
   crateShop,
@@ -123,7 +102,6 @@ const shopController = {
   toggleShopStatus,
   shopDetails,
   getAllShops,
-  // addProductInShop,
 };
 
 module.exports = shopController;
